b5eb75d695759419ce6c16483c1f105adb5c87a6
marcush12 <marcos012santos@gmail.com>
2018-02-26 20:48:04 -0300

    pronto

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/google/redirect',
+            'scope' => [],
+        ],
+        'github' => [
+            'client_id' => '6768e23c352d54e843c4',
+            'client_secret' => '2bbab8fa4d1977271cd336456530a10ebd1e4432',
+            'redirect_uri' => 'http://forum.stu/github/redirect',
+            'scope' => [],
+        ],
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserstableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => 'admin',
+            'password' => bcrypt('admin'),
+            'email' => 'admin@admin.com',
+            'admin' => 1,
+            'avatar' => asset('avatars/avatar.png')
+        ]);
+
+        App\User::create([
+            'name' => 'Emily Myers',
+            'password' => bcrypt('password'),
+            'email' => 'emily@myers.com',
+            'avatar' => asset('avatars/avatar.png')
+        ]); 
+    }
+}
+
